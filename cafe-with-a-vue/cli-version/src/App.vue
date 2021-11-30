0eee92cc6b3bab47a0b17c6f453253b93bf52891
TREMOULET Aymeric <SP003C76@eu.airbus.corp>
2021-11-30 15:58:26 +0100

    Done

---
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import NavLink from './components/NavLink.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+	name: 'App',
+	components: {
+		NavLink,
+    HelloWorld
+	}
+}
+</script>
+
+<template>
+	<div id="app">
+		<nav>
+			<NavLink url="/" text="Accueil"/>
+      <NavLink url="/about" text="À propos"/>
+      <NavLink url="/contact" text="Contact"/>
+		</nav>
+		<p>Bienvenue sur notre page d'accueil</p>
+    <HelloWorld msg="Cafe with a Vue"/>
+	</div>
+</template>
